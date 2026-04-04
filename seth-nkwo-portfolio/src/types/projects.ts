ecb0b2592c5f7a7e5a124e17f6378b05c6f6c471
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-04 22:09:25 +0100

    Update projects.ts

--- seth-nkwo-portfolio/src/types/projects.ts
+++ seth-nkwo-portfolio/src/types/projects.ts
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from "react";
+
 // Interface for project
 export interface Project {
     id: string;
@@ -20,4 +22,10 @@ export interface Project {
 export interface ProjectDetailModalProps {
     project: Project | null;
     onClose: () => void;
+}
+
+// Interface for project section props
+export interface ProjectSectionProps {
+    selectedProject: Project | null;
+    setSelectedProject: Dispatch<SetStateAction<Project | null>>;
 }
