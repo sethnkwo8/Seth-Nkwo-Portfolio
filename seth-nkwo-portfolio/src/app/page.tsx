3541d55a3b67d4d9930e6a2f615d1fdb95452765
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-04 21:59:26 +0100

    Update page.tsx

--- seth-nkwo-portfolio/src/app/page.tsx
+++ seth-nkwo-portfolio/src/app/page.tsx
@@ -1,23 +1,8 @@
 // Home Page
-import { About } from "@/components/AboutSection";
-import { ContactSection } from "@/components/ContactSection";
-import { HeroSection } from "@/components/HeroSection";
-import { ProjectsSection } from "@/components/ProjectsSection";
-import { SkillsSection } from "@/components/SkillsSection";
+import { Home } from "@/components/Home";
 
 export default function HomePage() {
   return (
-    <>
-      {/* Hero Section */}
-      <HeroSection />
-      {/* About Section */}
-      <About />
-      {/* Projects Section */}
-      <ProjectsSection />
-      {/* Skills Section */}
-      <SkillsSection />
-      {/* Contact Section */}
-      <ContactSection />
-    </>
+    <Home />
   )
 }
